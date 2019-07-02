6fad11b41ca6db7250308eccf55af95061d91c32
Adi <adi.mustaqim@students.amikom.ac.id>
2019-07-02 14:55:45 +0700

    Latihan Komposisi

---
+++ Mobil.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanKomposisi
+{
+    class Mobil
+    {
+        public string Merk { get; set; }
+        private Mesin mesin;
+
+        public Mobil()
+        {
+            // hubungan komposisi
+            mesin = new Mesin();
+            mesin.Type = "INLINE";
+        }
+
+        public Mesin GetInfoMesin()
+        {
+            return mesin;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanKomposisi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Demo Relasi Komposisi";
+
+            // membuat objek mobil
+            Mobil mobil = new Mobil();
+            mobil.Merk = "BMW";
+
+            Console.WriteLine("Merk mobil : {0}, Tipe mesin : {1}", mobil.Merk, mobil.GetInfoMesin().Type);
+            Console.ReadKey();
+        }
+    }
+}
